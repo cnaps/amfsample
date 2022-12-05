be07f867ff21680f313fb6f074a976725c8a6f08
cnaps <scant10@gmail.com>
2022-12-05 21:03:29 +0900

    init sample

---
+++ src/main/java/com/example/amf/AmfApplication.java
@@ -0,0 +1,13 @@
+package com.example.amf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/amf/AmfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
